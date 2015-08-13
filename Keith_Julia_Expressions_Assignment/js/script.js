f2fca11a1b6ef3d013480394033f070e00a0c00d
JPKeith <jpkeith@fullsail.edu>
2015-08-12 23:05:38 -0400

    First part of the expressions assignment

--- Keith_Julia_Expressions_Assignment/js/script.js
+++ Keith_Julia_Expressions_Assignment/js/script.js
@@ -3,4 +3,24 @@
  8/12/15
  Expressions Assignment
  */
-alert("My Java");
+
+// Variables
+var yourFirstName;
+var yourLastName;
+var yourAge;
+var petAge;
+
+//prompts
+yourFirstName = prompt("What is your first name?");
+yourLastName = prompt("What is your last name?");
+yourAge = prompt("What is your age?");
+anyPets = prompt ("Do you have any pets?");
+yourAge = Number(yourAge);
+petAge = prompt ("What is your pets age?");
+petAge = Number(petAge);
+
+
+// outputs
+console.log(" Hi, " + yourFirstName + " " + yourLastName + "." );
+console.log("You will be" + " " + (yourAge + 5) + " " +  "years old in five years.");
+console.log("Your age times five will be" + " " + (yourAge * 5));
